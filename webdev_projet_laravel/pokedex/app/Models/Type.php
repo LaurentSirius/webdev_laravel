193768ba2b2a3ea9e635ddf16f8cc7c55747137c
Lo_h <Hereisak@gmail.com>
2025-11-13 15:45:15 +0100

    Refactor Type model and add fillable attributes
    Refactored the Type model to use manual incrementing for the primary key, set the key type to bigint, and added the fillable attributes for mass assignment. Removed the many-to-many relationship methods for Pokemon and Trainer.

--- webdev_projet_laravel/pokedex/app/Models/Type.php
+++ webdev_projet_laravel/pokedex/app/Models/Type.php
@@ -5,23 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class Type extends Model
-{
+class Type extends Model {
     protected $table = 'types';
     protected $primaryKey = 'id';
+    public $incrementing = false; // important car id est manuel
+    protected $keyType = 'bigint';
 
-
-    #region RELATIONS
-    // Relation many-to-many avec la table types (au travers de la table pokemon_types_weaknesses)
-    public function pokemon(): BelongsToMany
-    {
-        return $this->belongsToMany(Pokemon::class, 'pokemon_types_weaknesses')
-            ->withPivot('is_weakness');
-    }
-
-    public function trainers(): BelongsToMany
-    {
-        return $this->belongsToMany(Trainer::class);
-    }
-    #endregion
+    // AJOUTE ÇA :
+    protected $fillable = ['id', 'name', 'color_code'];
 }
